a3f04692bd1e079c2d79749560ddb7d08e5d58e5
Renzo00023 <160646514+Renzo00023@users.noreply.github.com>
2024-06-20 17:10:08 -0300

    Add files via upload

---
+++ Actividad 5.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Resultado Actividad 5</title>
+</head>
+<body>
+    <?php
+    $jugador = array("González", "Pérez", "Rodríguez", "López", "Martínez");
+    echo "La alineación del equipo está compuesta por: " . implode(", ", $jugador) . ".<br>";
+    ?>
+</body>
+</html>
